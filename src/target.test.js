f36b1db1dd8ba09ccd92b99e4c7e043deb80e652
Yasushi20260708 <takehara@liberty-it.biz>
2026-07-10 14:28:48 +0900

    update

---
+++ src/target.test.js
@@ -0,0 +1,13 @@
+const test = require('node:test');
+const assert = require('node:assert/strict');
+const { add } = require('./math');
+
+test('2つの数値を足し合わせる', () => {
+    // add(1, 2) の結果が 3 であることを検証
+    assert.strictEqual(add(1, 2), 3);
+});
+
+test('マイナスの数値を足し合わせる', () => {
+    // add(-1, 1) の結果が 0 であることを検証
+    assert.strictEqual(add(-1, 1), 0);
+});
